ddb47b9a546523a4bc5a3e0532c56762347251e4
Bob Brown <robertbernardbrown@gmail.com>
2018-02-07 20:21:17 -0500

    finishes basic funcionality of homework

--- index.js
+++ index.js
@@ -12,92 +12,119 @@ function initialPrompt() {
     }]).then(answers => {
 
         if (answers.confirmation) {
-            var allTheWords = ['Dude', 'Manny', 'Hot Rod', 'Moana', 'Harry'];
-            var randomNum = Math.floor(Math.random() * 4);
-            var theWord = allTheWords[randomNum];
-            var wordConst = new Word.Word(theWord);
-            var chances = 10;
-            var alphabet = 'qwertyuiopasdfghjklzxcvbnm'
-            var alphabetArr = alphabet.split('')
-            console.log(theWord, wordConst)
+            var allTheWords = ['Never', 'Gonna', 'Give', 'You', 'Up'];
 
-            function nextPrompt() {
-                if (wordConst.underscores() !== theWord && chances > 0) {
-                    inquirer.prompt([{
-                        type: 'input',
-                        name: 'inputVal',
-                        message: 'Guess a letter!'
-                    }]).then(answers => {
+            function runGame() {
+                var randomNum = Math.floor(Math.random() * (allTheWords.length - 1));
+                console.log(randomNum, allTheWords)
+                var theWord = allTheWords[randomNum];
+                var wordConst = new Word.Word(theWord);
+                var chances = 10;
+                var alphabet = 'qwertyuiopasdfghjklzxcvbnm'
+                var alphabetArr = alphabet.split('')
+                console.log(theWord, wordConst)
 
-                        var flag = false;
-                        for (var i = 0; i < alphabetArr.length; i++) {
-                            var element = alphabetArr[i];
-                            if (element === answers.inputVal) {
-                                alphabetArr.splice(i, 1)
-                                flag = true;
+                function nextPrompt() {
+                    if (wordConst.underscores() !== theWord && chances > 0) {
+                        inquirer.prompt([{
+                            type: 'input',
+                            name: 'inputVal',
+                            message: 'Guess a letter!'
+                        }]).then(answers => {
+
+                            var flag = false;
+                            for (var i = 0; i < alphabetArr.length; i++) {
+                                var element = alphabetArr[i];
+                                if (element === answers.inputVal) {
+                                    alphabetArr.splice(i, 1)
+                                    flag = true;
+                                }
                             }
-                        }
-                        if (flag) {
-                            displayWord(answers.inputVal);
-                            correctResponse(answers.inputVal);
-                            nextPrompt();
+                            if (flag) {
+                                displayWord(answers.inputVal);
+                                correctResponse(answers.inputVal);
+                                nextPrompt();
+                            } else {
+                                console.log("Please choose a different letter.")
+                                displayWord(answers.inputVal);
+                                nextPrompt();
+                            }
+
+                        })
+                    } else if (wordConst.underscores() !== theWord && chances === 0) {
+                        gameOverCheck(chances);
+                    } else {
+
+                        console.log(allTheWords.length)
+
+                        if ((allTheWords.length - 1) > 0) {
+                            console.log('You guessed the word! Onto the next one')
+                            allTheWords.splice(randomNum, 1);
+                            console.log(allTheWords)
+                            runGame();
                         } else {
-                            console.log("Please choose a different letter.")
-                            displayWord(answers.inputVal);
-                            nextPrompt();
+
+                            inquirer.prompt([{
+                                type: 'confirm',
+                                name: 'playAgain',
+                                message: "Congrats! That's all the words. You're never gonna let me down! \n Play again?"
+                            }]).then(answers => {
+                                if (answers.playAgain) {
+                                    initialPrompt();
+                                } else {
+                                    console.log('No prob, come back later!')
+                                }
+                            })
                         }
 
-                    })
-                } else if (wordConst.underscores() !== theWord && chances === 0) {
-                    gameOverCheck(chances);
-                } else {
-                    console.log('You guessed the word!')
-                    allTheWords.splice(randomNum, 1);
-                    console.log(allTheWords)
-                }
-            }
-
-            function displayWord(letter) {
-                wordConst.guess(letter);
-                console.log(wordConst.underscores());
-            }
-
-            function correctResponse(letter) {
-                var flag = false;
-                for (var i = 0; i < wordConst.word.length; i++) {
-                    var element = wordConst.word[i].characterVal;
-                    if (letter === element || letter.toUpperCase() === element) {
-                        flag = true;
                     }
                 }
-                if (flag) {
-                    console.log("Correct!");
-                } else {
-                    console.log("Good try! Try Again");
-                    chances--
-                    console.log("You have " + chances + " chances left!");
-                }
-            }
 
-            function gameOverCheck(chanceVar) {
-                if (chanceVar === 0) {
-                    inquirer.prompt([{
-                        type: 'confirm',
-                        name: 'playAgain',
-                        message: 'GAME OVER! Play again?'
-                    }]).then(answers => {
-                        if (answers.playAgain) {
-                            console.log(wordConst)
-                            initialPrompt();
-                        } else {
-                            console.log('No prob, come back later!')
+                function displayWord(letter) {
+                    wordConst.guess(letter);
+                    console.log(wordConst.underscores());
+                }
+
+                function correctResponse(letter) {
+                    var flag = false;
+                    for (var i = 0; i < wordConst.word.length; i++) {
+                        var element = wordConst.word[i].characterVal;
+                        if (letter === element || letter.toUpperCase() === element) {
+                            flag = true;
                         }
-                    })
+                    }
+                    if (flag) {
+                        console.log("Correct!");
+                    } else {
+                        console.log("Good try! Try Again");
+                        chances--
+                        console.log("You have " + chances + " chances left!");
+                    }
                 }
+
+                function gameOverCheck(chanceVar) {
+                    if (chanceVar === 0) {
+                        inquirer.prompt([{
+                            type: 'confirm',
+                            name: 'playAgain',
+                            message: 'GAME OVER! Play again?'
+                        }]).then(answers => {
+                            if (answers.playAgain) {
+                                console.log(wordConst)
+                                initialPrompt();
+                            } else {
+                                console.log('No prob, come back later!')
+                            }
+                        })
+                    }
+                }
+
+                displayWord('');
+                nextPrompt()
+
             }
 
-            displayWord('');
-            nextPrompt()
+            runGame();
 
         } else {
             console.log('No sweat! Come back later if you decide you want to play.')
